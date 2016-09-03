cc9c7f66fea86f42bd8d292b6495a5049619f706
suissa <jnascimento@gmail.com>
2016-09-03 11:42:38 -0300

    amem

--- integral.js
+++ integral.js
@@ -1,32 +1,30 @@
 // PoC de Fórmula das Primitivas
 // https://pt.wikipedia.org/wiki/Integral
+// Porém ainda precisa ser melhorado
+
 
 const transformeX = (nums) => nums[0]/nums[1] 
-const multiplicaPorFracao = (nums) => nums[0]*(1/nums[1])
 
-const calcula = (nums) => {
-  if(nums.length === 1) return [parseInt(nums[0]), 1]
-  return [parseInt(nums[0]), parseInt(nums[1])+1]
-}
+const calcula = (nums) => (nums.length === 1) 
+  ? [parseInt(nums[0]), 1]
+  : [parseInt(nums[0]), parseInt(nums[1])+1]
 
 const extraiNumeros = (equacao) => {
-  let calc = []
+  const calc = []
   equacao.forEach((num,i) => calc.push(num.split('x')))
   return calc 
 }
 
 const reduzirIntegral = (equacao) => {
-  let calc = []
+  const calc = []
   equacao.forEach((num,i) => calc.push(calcula(num)))
   return calc
 }
 
 const calculeIntegral = (equacao) => {
-  let xs = []
-  let multiplicacao = []
+  const xs = []
   equacao.forEach((num, i) => {
     xs.push(transformeX(num))
-    multiplicacao.push(multiplicaPorFracao(num))
   })
 
   return (x, expoente, i) => {
@@ -35,11 +33,12 @@ const calculeIntegral = (equacao) => {
     return resultado 
   }
 }
+
 const integralDefinida = (equacao, lim) => {
-  let calculos = []
-  let reduzida = reduzirIntegral(extraiNumeros(equacao))
+  const calculos = []
+  const reduzida = reduzirIntegral(extraiNumeros(equacao))
   // Acha a fórmula da Integral
-  let resolveX = calculeIntegral(reduzida)
+  const resolveX = calculeIntegral(reduzida)
   // resolve o X com os valores do limite
   lim.forEach((limite, i) => calculos.push(resolveX(limite, reduzida, i)))
   return calculos
